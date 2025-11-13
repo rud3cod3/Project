3ea34c228d0120908b83ca3c49cf9550688b8880
rud3cod3 <devops6722@gmail.com>
2025-11-13 17:18:00 +0530

    Bash file automating task

---
+++ Project_01/bash/UbuntuServer 22.04 LTS M1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Update linux and install nginx + unzip
+sudo apt update -y
+sudo apt install nginx unzip -y
+
+# Start, enable & nginx
+nginx_func() {
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+}
+
+# Now checking nginx
+nginx_status=$(systemctl is-active nginx)
+
+if [ "$nginx_status" = "active" ]; then
+    echo "Nginx is running!"
+else
+    echo "Nginx is not running. Trying to start it..."
+    nginx_func
+fi
+
+# Download, Extract and Copy Code 
+sudo curl -L https://www.tooplate.com/zip-templates/2097_pop.zip -o /opt/2097_pop.zip
+sudo unzip -o /opt/2097_pop.zip -d /opt/2097_pop/
+sudo rm -rf /var/www/html/*
+sudo cp -r /opt/2097_pop/* /var/www/html/
+
+# Restart nginx
+sudo systemctl restart nginx
